d9d16067f7bd97bf2f665d291434072cea8df59e
shivangi1612 <shivangi.singh161204@gmail.com>
2025-02-26 11:23:14 +0530

    Final Commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {
+      animation: {
+        'fade-in': 'fadeIn 0.5s ease-in',
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+      },
+    },
+  },
+  plugins: [],
+}
